4d05bc1c8070500e51498f02c6249b5a27c79dce
Rob Sutter <rob.sutter@fauna.com>
2021-10-19 10:45:39 -0400

    Added worktop and first route - POST /listing

--- wrangler/index.js
+++ wrangler/index.js
@@ -1,12 +1,45 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
-}
+// Copyright Fauna, Inc.
+// SPDX-License-Identifier: MIT-0
+
+import { Router, listen } from 'worktop';
+import faunadb from 'faunadb';
+import {customFetch, getFaunaError} from './utils.js';
+
+const router = new Router();
+
+const faunaClient = new faunadb.Client({
+  secret: FAUNA_SECRET,
+  fetch: customFetch
+});
+
+const { Call } = faunadb.query;
+
+router.add('GET', '/', async (request, response) => {
+  response.send(200, 'Hello, worktop!');
+});
+
+router.add('POST', '/listing', async (request, response) => {
+  const { title, description, reserve } = await request.body();
+
+  try {
+    const query = Call(
+      "CreateListing",
+      {
+        data: {
+          title: title,
+          description: description,
+          reserve: reserve
+        }
+      },
+    );
+    
+    const result = await faunaClient.query(query);
+  
+    response.send(200, result);  
+  } catch (error) {
+    const faunaError = getFaunaError(error);
+    response.send(faunaError.status, faunaError);
+  };
+});
+
+listen(router.run);
---
+++ wrangler/utils.js
@@ -0,0 +1,41 @@
+// Copyright Fauna, Inc.
+// SPDX-License-Identifier: MIT-0
+
+export function customFetch (url, params) {
+  const signal = params.signal;
+  delete params.signal;
+
+  const abortPromise = new Promise((resolve) => {
+    if (signal) {
+      signal.onabort = resolve
+    }
+  });
+
+  return Promise.race([abortPromise, fetch(url, params)])
+}
+
+export function getFaunaError (error) {
+
+  const {code, description} = error.requestResult.responseContent.errors[0];
+  let status;
+
+  switch (code) {
+    case 'instance not found':
+      status = 404;
+      break;
+    case 'instance not unique':
+      status = 409;
+      break;
+    case 'permission denied':
+      status = 403;
+      break;
+    case 'unauthorized':
+    case 'authentication failed':
+      status = 401;
+      break;
+    default:
+      status = 500;
+  }
+
+  return {code, description, status};
+}
